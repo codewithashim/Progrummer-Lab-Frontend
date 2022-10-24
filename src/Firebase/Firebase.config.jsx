04abac89ab8e9ecec0ccb4ed982f953b37f47345
codewithashim <codewithashim@gmail.com>
2022-10-24 21:46:24 +0600

    added clinte side

---
+++ src/Firebase/Firebase.config.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0JJDK-tnKlctQkDkdaO8DZgui_bOUiCU",
+  authDomain: "programmer-lab.firebaseapp.com",
+  projectId: "programmer-lab",
+  storageBucket: "programmer-lab.appspot.com",
+  messagingSenderId: "880683543510",
+  appId: "1:880683543510:web:a606138be0ebf8c3cb5277",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
