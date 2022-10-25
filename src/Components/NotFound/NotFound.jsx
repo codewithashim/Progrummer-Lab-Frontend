f3e624c91adfb0f33e3c6253fdf04740a3df73d0
codewithashim <codewithashim@gmail.com>
2022-10-25 15:34:49 +0600

    add error page

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
+import NotFoundPic from "../../Assects/NotFound.svg";
+import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-        <>
-            <section>
-                
-            </section>   
-        </>
-    );
+  return (
+    <section className="notFoundSection">
+      <div>
+        <h1 className="p-4 text-5xl font-bold">404 Not Found Data</h1>
+      </div>
+      <img src={NotFoundPic} alt="Not Found" />
+    </section>
+  );
 };
 
 export default NotFound;
--- src/Components/Router/PrivetRouter.jsx
+++ src/Components/Router/PrivetRouter.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { AuthContext } from "../../Context/AuthProvider";
 
 const PriveteRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
